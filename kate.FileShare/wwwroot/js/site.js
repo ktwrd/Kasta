bc7e6ad668478cefe2af185d9e9841d76f561a63
kate <kate@dariox.club>
2024-11-28 12:28:34 +0800

    [Home/Upload] Add dropzone library

--- kate.FileShare/wwwroot/js/site.js
+++ kate.FileShare/wwwroot/js/site.js
@@ -1,4 +1,45 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var fileshare = {
+    flightcheck: () => {
+        Dropzone.options.uploadDropzone = {
+            paramName: 'file',
+            maxFilesize: window.UserLimits.MaxUploadQuota / Math.pow(1024, 2),
+            error: function (file, response) {
+                this.defaultOptions.error(file, response.message);
+            },
+            totaluploadprogress: function (uploadProgress) {
+                var text = Math.round(uploadProgress) + '%';
+                $('#uploadProgess').css({'width': text}).text(text);
+            },
+            queuecomplete: function () {
+                $('#uploadProgess').css({'width': '0%'}).text('');
+            },
+            success: function (file, response) {
+                $(file.previewElement)
+                    .find('.dz-filename')
+                    .children()
+                    .html('<a href="' + response.url + '">' + file.name + '</a>');
+            },
+            timeout: 0
+        }
+    },
+    run: () => {
+        if ($('.dropzone').length > 0) {
+            fileshare.initClipboardPasteUpload();
+        }
+    },
+    initClipboardPasteUpload: function() {
+        document.onpaste = function(event){
+            if (event.clipboardData || event.originalEvent.clipboardData) {
+                const items = (event.clipboardData || event.originalEvent.clipboardData).items;
+                items.forEach((item) => {
+                    if (item.kind === 'file') {
+                        Dropzone.forElement('.dropzone').addFile(item.getAsFile());
+                    }
+                });
+            }
+        }
+    },
+}
 
-// Write your JavaScript code.
+fileshare.flightcheck();
+$(document).ready(fileshare.run);
