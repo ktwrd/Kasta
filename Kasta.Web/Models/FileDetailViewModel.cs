d9147979eb65fa5ddf37b0cbbc476b3fa3ccbe22
kate <kate@dariox.club>
2024-12-04 14:32:51 +0800

    Created FileDetailViewModel.cs

---
+++ Kasta.Web/Models/FileDetailViewModel.cs
@@ -0,0 +1,9 @@
+using Kasta.Data.Models;
+
+namespace Kasta.Web.Models;
+
+public class FileDetailViewModel
+{
+    public required FileModel File { get; set; }
+    public string? PreviewContent { get; set; }
+}
