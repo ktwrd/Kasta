d10966380603cf023001a4b4f53543140c8a93db
kate <kate@dariox.club>
2024-12-14 15:59:27 +0800

    Created TextViewComponent

---
+++ Kasta.Web/Models/Components/TextViewComponentModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Kasta.Web.Models.Components;
+
+public class TextViewComponentModel
+{
+    public string? Content { get; set; }
+    [DefaultValue(TextDisplayKind.Plain)]
+    public TextDisplayKind Kind { get; set; } = TextDisplayKind.Plain;
+    public string? CodeBlockClass { get; set; }
+}
+public enum TextDisplayKind
+{
+    Plain,
+    Html,
+    Markdown,
+    MarkdownBasic,
+    CodeBlock
+}
---
+++ Kasta.Web/ViewComponents/TextViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Kasta.Web.Models.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kasta.Web.ViewComponents;
+
+public class TextViewComponent : ViewComponent
+{
+    public Task<IViewComponentResult> InvokeAsync(TextViewComponentModel model)
+    {
+        return Task.Run(() => (IViewComponentResult)View("Default", model));
+    }
+}
