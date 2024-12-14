197b56d7adac13835eb709504ea035801f6d7666
kate <kate@dariox.club>
2024-12-14 17:06:06 +0800

    [EditDetailsComponentViewModel] Extend BaseAlertViewModel

--- Kasta.Web/Areas/Admin/Models/User/EditDetailsComponentViewModel.cs
+++ Kasta.Web/Areas/Admin/Models/User/EditDetailsComponentViewModel.cs
@@ -1,33 +1,11 @@
+using System.Collections.ObjectModel;
+using Kasta.Data.Models;
+using Kasta.Web.Models;
+
 namespace Kasta.Web.Areas.Admin.Models.User;
 
-public class EditDetailsComponentViewModel
+public class EditDetailsComponentViewModel : BaseAlertViewModel
 {
     public required string UserId { get; set; }
     public UserLimitModel? Limit { get; set; }
-
-    
-    public string? AlertType { get; set; }
-    public string? AlertClass
-    {
-        get
-        {
-            if (string.IsNullOrEmpty(AlertType))
-                return null;
-            
-            var t = AlertType.Trim().ToLower();
-            if (ValidAlertTypes.Contains(t))
-                return $"alert alert-{t}";
-            return "alert alert-secondary";
-        }
-    }
-    private static ReadOnlyCollection<string> ValidAlertTypes => new List<string>()
-    {
-        "primary",
-        "secondary",
-        "success",
-        "danger",
-        "warning",
-        "info"
-    }.AsReadOnly();
-    public string? AlertContent { get; set; }
 }
