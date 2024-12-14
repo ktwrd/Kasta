e467d7626e5ae2e130de7ad51e1526025240d60d
kate <kate@dariox.club>
2024-12-14 17:06:21 +0800

    [RoleDetailsComponentViewModel] Extend BaseAlertViewModel

--- Kasta.Web/Areas/Admin/Models/User/RoleDetailsComponentViewModel.cs
+++ Kasta.Web/Areas/Admin/Models/User/RoleDetailsComponentViewModel.cs
@@ -1,8 +1,9 @@
 using System.Collections.ObjectModel;
+using Kasta.Web.Models;
 
 namespace Kasta.Web.Areas.Admin.Models.User;
 
-public class RoleDetailsComponentViewModel
+public class RoleDetailsComponentViewModel : BaseAlertViewModel
 {
     public required string UserId { get; set; }
 
@@ -14,29 +15,4 @@ public class RoleDetailsComponentViewModel
     /// </summary>
     
     public Dictionary<string, string> Roles { get; set; } = [];
-    
-    public string? AlertType { get; set; }
-    public string? AlertClass
-    {
-        get
-        {
-            if (string.IsNullOrEmpty(AlertType))
-                return null;
-            
-            var t = AlertType.Trim().ToLower();
-            if (ValidAlertTypes.Contains(t))
-                return $"alert alert-{t}";
-            return "alert alert-secondary";
-        }
-    }
-    private static ReadOnlyCollection<string> ValidAlertTypes => new List<string>()
-    {
-        "primary",
-        "secondary",
-        "success",
-        "danger",
-        "warning",
-        "info"
-    }.AsReadOnly();
-    public string? AlertContent { get; set; }
 }
