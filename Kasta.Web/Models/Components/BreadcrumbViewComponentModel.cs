821e09487bb38e8b810055c0f4d775140981d846
kate <kate@dariox.club>
2024-12-14 17:05:24 +0800

    Created BreadcrumbViewComponent

---
+++ Kasta.Web/Models/Components/BreadcrumbViewComponentModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Kasta.Web.Models.Components;
+
+public class BreadcrumbViewComponentModel : List<BreadcrumbViewComponentItemModel>
+{
+}
+
+public class BreadcrumbViewComponentItemModel
+{
+    public required string Text { get; set; }
+    public string? TextIcon { get; set; }
+    public TextDisplayKind TextDisplay { get; set; } = TextDisplayKind.Plain;
+    public string? Link { get; set; }
+    public bool Current { get; set; }
+
+    public string ComputedClass
+    {
+        get
+        {
+            if (Current)
+            {
+                return "breadcrumb-item active";
+            }
+
+            return "breadcrumb-item";
+        }
+    }
+}
+
---
+++ Kasta.Web/ViewComponents/BreadcrumbViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Kasta.Web.Models.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kasta.Web.ViewComponents;
+
+public class BreadcrumbViewComponent : ViewComponent
+{
+    public Task<IViewComponentResult> InvokeAsync(List<BreadcrumbViewComponentItemModel> model)
+    {
+        return Task.Run(() => (IViewComponentResult)View("Default", model));
+    }
+}
