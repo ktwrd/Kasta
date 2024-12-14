47d4d803e4b39716f00b7e29c4a50c47252c1250
kate <kate@dariox.club>
2024-12-14 17:05:34 +0800

    Created AlertViewComponent

---
+++ Kasta.Web/Models/BaseAlertViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Kasta.Web.Models;
+
+public class BaseAlertViewModel
+{
+    public string? AlertType { get; set; }
+    public string? AlertClass
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(AlertType))
+                return null;
+            
+            var t = AlertType.Trim().ToLower();
+            if (ValidAlertTypes.Contains(t))
+                return $"alert alert-{t}";
+            return "alert alert-secondary";
+        }
+    }
+    private static ReadOnlyCollection<string> ValidAlertTypes => new List<string>()
+    {
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info"
+    }.AsReadOnly();
+    public string? AlertContent { get; set; }
+    [DefaultValue(true)]
+    public bool ShowAlertCloseButton { get; set; } = true;
+    [DefaultValue(true)]
+
+    public bool AlertContentAsMarkdown { get; set; } = true;
+}
---
+++ Kasta.Web/ViewComponents/AlertViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Kasta.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kasta.Web.ViewComponents;
+
+public class AlertViewComponent : ViewComponent
+{
+    public Task<IViewComponentResult> InvokeAsync(BaseAlertViewModel model)
+    {
+        return Task.Run(() => (IViewComponentResult)View("Default", model));
+    }
+}
