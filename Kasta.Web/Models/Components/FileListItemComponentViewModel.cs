4c3f6ff9cbd1e26e6876d782d1c5c1e0b328ec4f
kate <kate@dariox.club>
2024-12-16 16:27:01 +0800

    Add methods on FileListItemComponentViewModel for generating URL for delete & change state buttons.

--- Kasta.Web/Models/Components/FileListItemComponentViewModel.cs
+++ Kasta.Web/Models/Components/FileListItemComponentViewModel.cs
@@ -1,9 +1,38 @@
 using Kasta.Data.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Kasta.Web.Models.Components;
 
 public class FileListItemComponentViewModel
 {
-    public FileModel File { get; set; }
-    public UserSettingModel UserSettings { get; set; }
+    public required FileModel File { get; set; }
+    public required UserSettingModel UserSettings { get; set; }
+
+    public virtual string DeleteFileUrl(IUrlHelper url, FileModel file, string targetReturnUrl)
+    {
+        return GenerateAction(url, "DeleteFile", "Home", new()
+        {
+            {"id", file.Id},
+            {"returnUrl", targetReturnUrl}
+        });
+    }
+    public virtual string ChangeFileStateUrl(IUrlHelper url, FileModel file)
+    {
+        return GenerateAction(url, "ChangeFilePublicState", "Home", new()
+        {
+            {"id", file.Id},
+            {"state", (!file.Public).ToString()}
+        });
+    }
+    public string GenerateAction(IUrlHelper url, string action, string controller, Dictionary<string, object> parameters)
+    {
+        var u = url.Action(action, controller, parameters);
+        if (string.IsNullOrEmpty(u))
+        {
+            var j = string.Join(", ", parameters.Select(e => $"{e.Key}, {e.Value}"));
+            var js = string.IsNullOrEmpty(j) ? "" : $" ({j})";
+            throw new InvalidDataException($"Failed to generate Action Url for {nameof(controller)}={controller}, {nameof(action)}={action}" + js);
+        }
+        return u;
+    }
 }
