bd0be9bb5ae637700d93dde928a274c30038b66f
kate <kate@dariox.club>
2024-12-29 13:56:38 +0800

    [Kasta.Shared] Created Extensions.cs

---
+++ Kasta.Shared/Helpers/Extensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Kasta.Shared.Helpers;
+
+public static class Extensions
+{
+    public static string FancyMaxLength(this string value, int maxLength = 50, bool includeEllipses = true)
+    {
+        if (value.Length < maxLength)
+            return value;
+
+        var targetLength = maxLength;
+        
+        if (maxLength >= 3)
+        {
+            targetLength -= 3;
+        }
+
+        var result = value[..targetLength];
+        if (includeEllipses)
+        {
+            result += "...";
+        }
+
+        return result;
+    }
+}
