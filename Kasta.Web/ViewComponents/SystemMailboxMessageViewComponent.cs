bafdebc8c37595bf7b0d0579e702a0680c1f8275
kate <kate@dariox.club>
2024-12-29 14:46:23 +0800

    Created SystemMailboxMessageViewComponent

---
+++ Kasta.Web/ViewComponents/SystemMailboxMessageViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Kasta.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kasta.Web.ViewComponents;
+
+public class SystemMailboxMessageViewComponent : ViewComponent
+{
+    public Task<IViewComponentResult> InvokeAsync(SystemMailboxMessageModel model)
+    {
+        return Task.Run(() => (IViewComponentResult)View("Default", model));
+    }
+}
