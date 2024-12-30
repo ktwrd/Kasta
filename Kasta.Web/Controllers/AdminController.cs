3c9cfced170a8bbfd51c545d40388f43607f3efe
kate <kate@dariox.club>
2024-12-30 16:46:46 +0800

    [AdminController] Use AuthorizeAttribute

--- Kasta.Web/Controllers/AdminController.cs
+++ Kasta.Web/Controllers/AdminController.cs
@@ -8,39 +8,17 @@ using Kasta.Web.Models.Admin;
 using Microsoft.EntityFrameworkCore;
 using Kasta.Web.Services;
 using NLog;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Kasta.Web.Controllers;
 
 [Route("~/Admin")]
-[AdminRequired]
+[Authorize(Roles = RoleKind.Administrator)]
 public class AdminController : Controller
 {
-    private readonly ApplicationDbContext _db;
-    private readonly FileService _fileService;
-    private readonly SignInManager<UserModel> _signInManager;
-    private readonly UserManager<UserModel> _userManager;
-    private readonly ILogger<AdminController> _logger;
-    
-    public AdminController(IServiceProvider services, ILogger<AdminController> logger)
-        : base()
-    {
-        _logger = logger;
-        _db = services.GetRequiredService<ApplicationDbContext>();
-        _fileService = services.GetRequiredService<FileService>();
-        _signInManager = services.GetRequiredService<SignInManager<UserModel>>();
-        _userManager = services.GetRequiredService<UserManager<UserModel>>();
-    }
-
-    [AuthRequired]
     [HttpGet("Audit")]
-    public async Task<IActionResult> AuditIndex()
+    public IActionResult AuditIndex()
     {
-        var user = await _userManager.GetUserAsync(User);
-        if (user == null || !user.IsAdmin)
-        {
-            return new RedirectToActionResult("Index", "Home", null);
-        }
-
-        return View();
+        return View("AuditIndex");
     }
 }
