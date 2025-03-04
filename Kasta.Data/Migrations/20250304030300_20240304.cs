8a033a8e4e7f42b6f1cc5b9b4718aefcbc7f1807
kate <kate@dariox.club>
2025-03-04 11:03:14 +0800

    Updated migrations

---
+++ Kasta.Data/Migrations/20250304030300_20240304.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kasta.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20240304 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
