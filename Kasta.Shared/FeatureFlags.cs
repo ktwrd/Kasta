91ade81fc603c2e37c738b042bd45e3f71bb50b7
kate <kate@dariox.club>
2025-12-13 09:23:34 +0800

    [FeatureFlags] Add "Keys" class for storing environment variable names

--- Kasta.Shared/FeatureFlags.cs
+++ Kasta.Shared/FeatureFlags.cs
@@ -29,12 +29,21 @@ public static class FeatureFlags
         return EnvironmentHelper.ParseInt(envKey, defaultValue);
     }
     #endregion
+    
     public static string Endpoint => KastaConfig.Instance.Endpoint;
 
-    public static string SentryDsn => ParseString("SentryDsn", "");
+    public static string SentryDsn => ParseString(Keys.SentryDsn, "");
+    public static string XmlConfigLocation => ParseString(Keys.XmlConfigLocation, RunningInDocker ? "/config/kasta.xml" : "./config.xml");
+    public static bool RunningInDocker => ParseBool(Keys.RunningInDocker, false);
+    public static bool ShowPrivateInformationWithAspNet => ParseBool(Keys.ShowPrivateInformationWithAspNet, false);
+    public static bool SuppressPendingModelChangesWarning => ParseBool(Keys.SuppressPendingModelChangesWarning, false);
 
-    public static string XmlConfigLocation => ParseString("CONFIG_LOCATION", RunningInDocker ? "/config/kasta.xml" : "./config.xml");
-    public static bool RunningInDocker => ParseBool("_KASTA_RUNNING_IN_DOCKER", false);
-    public static bool ShowPrivateInformationWithAspNet => ParseBool("AspNet_ShowPrivateInformation", false);
-    public static bool SuppressPendingModelChangesWarning => ParseBool("EF_SuppressPendingModelChangesWarning", false);
+    public static class Keys
+    {
+        public const string SentryDsn = "SentryDsn";
+        public const string XmlConfigLocation = "CONFIG_LOCATION";
+        public const string RunningInDocker = "_KASTA_RUNNING_IN_DOCKER";
+        public const string ShowPrivateInformationWithAspNet = "AspNet_ShowPrivateInformation";
+        public const string SuppressPendingModelChangesWarning = "EF_SuppressPendingModelChangesWarning";
+    }
 }
