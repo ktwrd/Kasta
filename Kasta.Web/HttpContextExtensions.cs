9955f0726d09d6e940476066df1d9bacf6a0f90a
kate <kate@dariox.club>
2026-08-20 16:19:06 +0800

    HttpContextExtensions: Add support for multiple headers in IsHtmxRequest (because why not?)

--- Kasta.Web/HttpContextExtensions.cs
+++ Kasta.Web/HttpContextExtensions.cs
@@ -4,12 +4,10 @@ public static class HttpContextExtensions
 {
     public static bool IsHtmxRequest(this HttpContext context)
     {
-        var hxRequest = context.Request.Headers
+        return context.Request.Headers
             .Where(e => string.Equals(e.Key, "hx-request", StringComparison.OrdinalIgnoreCase))
-            .Select(e => e.Value)
-            .FirstOrDefault();
-        return hxRequest.Any(e
-            => string.Equals(e?.Trim(), "true", StringComparison.OrdinalIgnoreCase)
-            || (int.TryParse(e, out var ei) && ei >= 1));
+            .SelectMany(e => e.Value)
+            .Any(e => string.Equals(e?.Trim(), "true", StringComparison.OrdinalIgnoreCase)
+                      || (int.TryParse(e, out var ei) && ei >= 1));
     }
 }
